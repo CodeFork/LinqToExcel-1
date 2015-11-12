2f9110e25649f434cdd33e6e914fed1232f1ef26
josuelopezv <josuelopezv@gmail.com>
2015-11-12 12:13:29 -0600

    minor fix

--- LinqToExcel/ExcelColumnAttribute.cs
+++ LinqToExcel/ExcelColumnAttribute.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExcelToLinq
 {
-   public class ExcelColumnAttribute : Attribute
+    public class ExcelColumnAttribute : Attribute
     {
         public ExcelColumnAttribute()
         {
--- LinqToExcel/README.md
+++
@@ -1,2 +0,0 @@
-﻿# LinqToExcel
-Excel to Linq (Entity) library used to convert excel files .xlsx (2007 and above) and .xls (older) to a object oriented object
